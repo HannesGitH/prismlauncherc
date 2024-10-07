ea395a75e8ac045f40b2ecd1503d08cc3a686a64
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-10-07 12:48:33 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -10,14 +10,19 @@
   outputs =
     { nixpkgs, prismlauncher_upstream, ... }:
     let
+      inherit (nixpkgs) lib;
+
       addPatches = pkg: patches:
         pkg.overrideAttrs
         (oldAttrs: { patches = (oldAttrs.patches or [ ]) ++ patches; });
+      systems = lib.systems.flakeExposed;
+
+      forAllSystems = lib.genAttrs systems;
     in rec
     {
-      packages = {
-        default = addPatches prismlauncher_upstream [ ./crack.patch ];
-      };
+      packages = forAllSystems (system: {
+        default = addPatches prismlauncher_upstream.packages.${system}.prismlauncher [ ./crack.patch ];
+      });
 
       overlays.default = final: prev: {
         prismlauncher = packages.default;
