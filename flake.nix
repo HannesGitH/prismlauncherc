43bfafef0aa9ba386c141b851f2a66557eb1fcde
Hannes <33062605+HannesGitH@users.noreply.github.com>
2024-10-07 14:22:02 +0200

    dynamic patching

--- flake.nix
+++ flake.nix
@@ -18,13 +18,29 @@
       systems = lib.systems.flakeExposed;
 
       forAllSystems = lib.genAttrs systems;
+
+      patch = system: (pkgs: src: pkgs.stdenvNoCC.mkDerivation {
+        name = "prism-launcher-patch";
+        src = src;
+        nativeBuildInputs = with pkgs; [ git gawk ];
+        buildPhase = ''
+          echo "patching"
+          awk '/bool AccountList::anyAccountIsValid\(\)/ { print; getline; print "{"; print "    return true;"; next }1' launcher/minecraft/auth/AccountList.cpp > temp
+          echo "generating patch"
+          git --no-pager diff --no-index launcher/minecraft/auth/AccountList.cpp temp > crack.patch || true
+          echo "done"
+        '';
+        installPhase = ''
+          cp crack.patch $out
+        '';
+      }) nixpkgs.legacyPackages.${system}.pkgs;
     in rec
     {
       packages = forAllSystems (system: {
         default = 
           (ups: 
             ups.prismlauncher.override {
-              prismlauncher-unwrapped = (addPatches ups.prismlauncher-unwrapped [ ./crack.patch ]);
+              prismlauncher-unwrapped = addPatches ups.prismlauncher-unwrapped [ (patch system ups.prismlauncher-unwrapped.src) ];
             }
           ) prismlauncher_upstream.packages.${system};
       });
