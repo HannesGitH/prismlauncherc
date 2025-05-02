3b7a9bef002a7817197215f5e616edec298fc01c
Hannes <33062605+HannesGitH@users.noreply.github.com>
2025-05-02 19:29:56 +0200

    working patch

--- flake.nix
+++ flake.nix
@@ -26,10 +26,10 @@
         buildPhase = ''
           echo "patching"
           awk '/bool AccountList::anyAccountIsValid\(\)/               { print; getline; print "{"; print "    return true;"; next }1' launcher/minecraft/auth/AccountList.cpp > temp_account_list
-          awk '/void AuthSession::MakeDemo\(QString name, QString u\)/ { print; getline; print "{"; print "    return;"     ; next }1' launcher/minecraft/auth/AuthSession.cpp > temp_auth_session
+          awk '/bool ownsMinecraft\(\) const/ { print "    bool ownsMinecraft() const { return true; }"; while(getline && !/^}/) {}; next }1' launcher/minecraft/auth/MinecraftAccount.h > temp_own_minecraft
           echo "generating patch"
-          git --no-pager diff --no-index launcher/minecraft/auth/AccountList.cpp temp_account_list > crack.patch || true
-          git --no-pager diff --no-index launcher/minecraft/auth/AuthSession.cpp temp_auth_session >> crack.patch || true
+          git --no-pager diff --no-index launcher/minecraft/auth/AccountList.cpp temp_account_list >> crack.patch || true
+          git --no-pager diff --no-index launcher/minecraft/auth/MinecraftAccount.h temp_own_minecraft >> crack.patch || true
           echo "done"
         '';
         installPhase = ''
